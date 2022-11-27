6e446d02d04bc8bf14cecee53acf69d619777708
Eric Haynes <ehaynes99@gmail.com>
2022-11-27 16:35:16 -0500

    Put globals back in root init.lua for now

--- init.lua
+++ init.lua
@@ -1,9 +1,14 @@
+vim.g.NVIM_CONFIG_DIR = os.getenv('MYVIMRC'):match('(.*[/\\])')
+vim.g.PLUGINS_DIR = vim.g.NVIM_CONFIG_DIR .. 'lua/plugins/'
+vim.g.COMPILED_PACKER_FILE = vim.g.NVIM_CONFIG_DIR .. 'plugin/packer_compiled.lua'
+
+require('options')
+require('keymaps')
+require('plugins')
+require('autocommands')
+
 -- setmetatable(_G, {
 --   __index = function(_, key)
 --     error('undefined variable `' .. key .. '` used', 2)
 --   end,
 -- })
-require('options')
-require('keymaps')
-require('plugins')
-require('autocommands')
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,3 @@
-vim.g.NVIM_CONFIG_DIR = os.getenv('MYVIMRC'):match('(.*[/\\])')
-vim.g.PLUGINS_DIR = vim.g.NVIM_CONFIG_DIR .. 'lua/plugins/'
-vim.g.COMPILED_PACKER_FILE = vim.g.NVIM_CONFIG_DIR .. 'plugin/packer_compiled.lua'
-
 -- Automatically install packer
 local ensure_packer = function()
   local fn = vim.fn
