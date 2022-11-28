51871da99e3479e5d5815ba74b5790e7e859ca2a
Eric Haynes <eric.haynes@openphone.co>
2022-11-28 12:08:18 -0500

    Put packer autocommand in packer init file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,18 +1,39 @@
--- Automatically install packer
 local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-    print('Installing packer.')
     vim.cmd([[packadd packer.nvim]])
+    return true
   end
-  return require('packer')
+  return false
 end
 
-local packer = ensure_packer()
+local packer_bootstrap = ensure_packer()
 
--- Have packer use a popup window
+local packer = require('packer')
+
+local compiled_packer_exists = function()
+  local f = io.open(vim.g.COMPILED_PACKER_FILE, 'r')
+  if f ~= nil then
+    io.close(f)
+    return true
+  end
+end
+
+-- remove compiled plugins when any file in `lua/plugins` changes
+vim.api.nvim_create_autocmd({ 'BufWrite' }, {
+  pattern = vim.g.PLUGINS_DIR .. '**',
+  callback = function(args)
+    local dirty = vim.fn.getbufinfo(args.buf)[1].changed ~= 0
+    if dirty and compiled_packer_exists() then
+      local notify = require('notify')
+      os.remove(vim.g.COMPILED_PACKER_FILE)
+      notify('Removed compiled plugins')
+    end
+  end,
+})
+
 packer.init({
   display = {
     open_fn = function()
@@ -27,8 +48,6 @@ packer.init({
 -- add disabled plugins here
 local ignored = {
   'init.lua',
-  'neo-tree.lua',
-  'cmp-nvim-lsp.lua',
 }
 
 local is_ignored = function(path)
@@ -43,8 +62,11 @@ return packer.startup(function(use)
   local paths = vim.split(vim.fn.glob(vim.g.PLUGINS_DIR .. '**/*.lua'), '\n')
   for _, path in pairs(paths) do
     if not is_ignored(path) then
-      -- if path ~= init_file then
       use(dofile(path))
     end
+  end
+  if packer_bootstrap or not compiled_packer_exists() then
+    print('synching packer')
+    require('packer').sync()
   end
 end)
