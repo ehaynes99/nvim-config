565315d017fc90ca199cccfe59f9bdd1e56198a1
Eric Haynes <eric.haynes@openphone.co>
2022-11-30 12:41:12 -0500

    Explicitly enable highlighting

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -3,16 +3,19 @@ local treesitter = require('nvim-treesitter.configs')
 
 treesitter.setup({
   ensure_installed = {
-    -- 'javascript',
-    -- 'typescript',
-    -- 'rust',
-    -- 'lua',
-    -- 'markdown',
-    -- 'markdown_inline',
-    -- 'bash',
-    -- 'python',
+    'javascript',
+    'typescript',
+    'rust',
+    'lua',
+    'markdown',
+    'markdown_inline',
+    'bash',
+    'python',
   },
   auto_install = true,
+  highlight = {
+    enable = true,
+  },
 
   -- from 'windwp/nvim-ts-autotag' plugin
   autotag = {
