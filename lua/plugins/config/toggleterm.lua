e2d0c08990ca28a63842dbbd2c66691ca2fa3fc4
Eric Haynes <eric.haynes@openphone.co>
2022-12-01 17:03:39 -0500

    Add terminals for jest testing and gitui

--- lua/plugins/config/toggleterm.lua
+++ lua/plugins/config/toggleterm.lua
@@ -1,14 +1,54 @@
-local term = require('toggleterm')
+local toggleterm = require('toggleterm')
+local Terminal = require('toggleterm.terminal').Terminal
+local js_utils = require('utils.javascript')
 
-term.setup({
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
+vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]])
+vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]])
+vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]])
+vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
+
+toggleterm.setup({
   size = 60,
   open_mapping = [[<c-\>]],
-  -- direction = 'vertical',
   direction = 'float',
-  -- shade_terminals = true,
-  -- shading_factor = 2,
   float_opts = {
     border = 'curved',
     width = 180,
   },
 })
+
+local gitui_term = Terminal:new({ cmd = 'gitui', hidden = true })
+
+vim.keymap.set('n', '<leader>G', function()
+  gitui_term:toggle()
+end)
+
+local jest_term = Terminal:new({
+  direction = 'vertical',
+  size = 90,
+  id = 9,
+  close_on_exit = true,
+  hidden = true,
+  -- on_exit = function(terminal, job, exit_code, name)
+  --   print('job: ' .. job .. 'exit code: ' .. exit_code .. 'name: ' .. name)
+  -- end,
+})
+
+vim.keymap.set('n', '<leader>t', function()
+  local test_file = vim.api.nvim_buf_get_name(0)
+  local project_root = js_utils.project_root(test_file)
+  if not project_root then
+    print('could not find project root: ' .. test_file)
+    return
+  end
+  local command = 'npx -y --node-options=--inspect jest --watch --no-coverage ' .. test_file
+
+  if jest_term:is_open() then
+    jest_term:close()
+  else
+    jest_term:open()
+    jest_term:change_dir(project_root)
+    toggleterm.exec(command, 9, 90, project_root)
+  end
+end)
