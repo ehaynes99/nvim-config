f028d266d60a14e963b6e8ed68ef168e2f3bf145
Eric Haynes <eric.haynes@openphone.co>
2022-12-07 21:58:54 -0500

    A few utils for buffers

--- lua/utils/editor.lua
+++ lua/utils/editor.lua
@@ -24,4 +24,33 @@ M.search_tree_dir = function()
   end
 end
 
+-- TODO - ensure that this is accurate
+M.buf_is_file = function(buf)
+  buf = buf or 0
+
+  return vim.api.nvim_buf_get_option(0, 'buftype') == ''
+end
+
+-- see `:h options` section '3. Options summary'
+M.buf_get_options = function(buf, option_names)
+  local result = {}
+  for _, option in pairs(option_names) do
+    result[option] = vim.api.nvim_buf_get_option(buf, option)
+  end
+  return result
+end
+
+-- adapted from: https://github.com/akinsho/bufferline.nvim/blob/4ecfa81e470a589e74adcde3d5bb1727dd407363/lua/bufferline/utils/init.lua#L132-L136
+-- The provided api nvim_is_buf_loaded filters out all hidden buffers
+function M.is_valid(buf_num)
+  if not buf_num or buf_num < 1 then return false end
+  local exists = vim.api.nvim_buf_is_valid(buf_num)
+  return vim.bo[buf_num].buflisted and exists
+end
+
+-- adapted from: https://github.com/akinsho/bufferline.nvim/blob/4ecfa81e470a589e74adcde3d5bb1727dd407363/lua/bufferline/utils/init.lua#L143
+M.get_valid_buffers = function()
+  return vim.tbl_filter(M.is_valid, vim.api.nvim_list_bufs())
+end
+
 return M
