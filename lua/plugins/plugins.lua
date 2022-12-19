aeea426e66e33ae1ef8c60a091585eaa24321140
Eric Haynes <ehaynes99@gmail.com>
2022-12-18 22:17:33 -0500

    Remove glow plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -43,7 +43,6 @@ return {
   { 'norcalli/nvim-colorizer.lua', commit = '36c610a9717cc9ec426a07c8e6bf3b3abcb139d6' },
   { 'mrjones2014/legendary.nvim', commit = 'b0e02000b3227e9e07adfd41565e82d166fad75e' },
   { 'chentoast/marks.nvim', commit = 'a69253e4b471a2421f9411bc5bba127eef878dc0' },
-  { 'ellisonleao/glow.nvim', commit = '20d1cd087f8728f21a048a3b6259f6177237b39e' },
   { 'kosayoda/nvim-lightbulb', commit = '56b9ce31ec9d09d560fe8787c0920f76bc208297' },
   { 'kazhala/close-buffers.nvim', commit = '3acbcad1211572342632a6c0151f839e7dead27f' },
   { 'lukas-reineke/indent-blankline.nvim', commit = 'db7cbcb40cc00fc5d6074d7569fb37197705e7f6' },
