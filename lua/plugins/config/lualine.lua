85c0fcb6cc464cc940343b9df44d6c42fcfe61b2
Eric Haynes <ehaynes99@gmail.com>
2022-12-19 18:06:12 -0500

    Add cwd in status line

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -1,11 +1,15 @@
 local lualine = require('lualine')
 
 lualine.setup({
-
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { { 'filename', newfile_status = true, path = 1 } },
+    lualine_c = {
+      function()
+        return vim.fn.getcwd()
+      end,
+      { 'filename', newfile_status = true, path = 1 },
+    },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
