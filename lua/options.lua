d3dc972505df49542e8ef3b11912ea92a2e00fac
Eric Haynes <ehaynes99@gmail.com>
2022-12-20 14:22:24 -0500

    Disable shada

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,4 @@
+vim.opt.shada = 'NONE'
 vim.opt.backup = false -- creates a backup file
 vim.opt.clipboard = 'unnamedplus' -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 1 -- prevent
