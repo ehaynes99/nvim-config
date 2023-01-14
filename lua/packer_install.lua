d362d83793ec69e722805cb8bf4648205af42c17
Eric Haynes <eric.haynes@openphone.co>
2023-01-13 20:34:27 -0500

    Temporarily remove luasnip

--- lua/packer_install.lua
+++ lua/packer_install.lua
@@ -67,9 +67,9 @@ return start({
   { 'olimorris/onedarkpro.nvim' },
 
   -- LSP & completion
-  { 'L3MON4D3/LuaSnip', commit = '8f8d493e7836f2697df878ef9c128337cbf2bb84' },
+  -- { 'L3MON4D3/LuaSnip', commit = '8f8d493e7836f2697df878ef9c128337cbf2bb84' },
   { 'jose-elias-alvarez/null-ls.nvim', commit = 'a0acd495f5edce6d4d5d3c6bd63d2319ccded9ed' },
-  { 'hrsh7th/nvim-cmp', commit = 'b0dff0ec4f2748626aae13f011d1a47071fe9abc' },
+  -- { 'hrsh7th/nvim-cmp', commit = 'b0dff0ec4f2748626aae13f011d1a47071fe9abc' },
   { 'saadparwaiz1/cmp_luasnip', commit = 'a9de941bcbda508d0a45d28ae366bb3f08db2e36' },
   { 'hrsh7th/cmp-nvim-lua', commit = 'd276254e7198ab7d00f117e88e223b4bd8c02d21' },
   { 'hrsh7th/cmp-buffer', commit = '3022dbc9166796b644a841a02de8dd1cc1d311fa' },
