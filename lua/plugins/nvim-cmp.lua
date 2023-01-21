80161fb4d23bd4793ed7e19674993476c30f01fe
Eric Haynes <eric.haynes@openphone.co>
2023-01-20 20:46:25 -0500

    Rework cmp setup

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,64 +1,77 @@
+local cmp_mapping = function()
+  local cmp = require('cmp')
+  local luasnip = require('luasnip')
+
+  -- from the wiki, but I can't tell what this does...
+  local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+  end
+
+  return cmp.mapping.preset.insert({
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    -- Prevent autocomplete unless explicitly tabbing into the list
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  })
+end
+
+local cmp_snippet = function()
+  local luasnip = require('luasnip')
+  local snipmate = require('luasnip.loaders.from_snipmate')
+
+  luasnip.setup({
+    history = false,
+  })
+
+  snipmate.lazy_load()
+
+  return {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  }
+end
+
 return {
   'hrsh7th/nvim-cmp',
   dependencies = {
-    'hrsh7th/cmp-nvim-lua',
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
-    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-cmdline',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
   },
   config = function()
     local cmp = require('cmp')
-    ---@diagnostic disable-next-line: different-requires
-    -- local luasnip = require('luasnip')
-
-    local has_words_before = function()
-      ---@diagnostic disable-next-line: deprecated
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-    end
 
     cmp.setup({
-      -- snippet = {
-      --   expand = function(args)
-      --     luasnip.lsp_expand(args.body)
-      --   end,
-      -- },
-
-      mapping = cmp.mapping.preset.insert({
-        ['<C-k>'] = cmp.mapping.select_prev_item(),
-        ['<C-j>'] = cmp.mapping.select_next_item(),
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-        ['<C-e>'] = cmp.mapping({
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        }),
-        -- Prevent autocomplete unless explicitly tabbing into the list
-        ['<CR>'] = cmp.mapping.confirm({ select = false }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          -- elseif luasnip.expand_or_locally_jumpable() then
-          --   luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete(nil)
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          -- elseif luasnip.jumpable(-1) then
-          --   luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-      }),
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp_mapping(),
+      snippet = cmp_snippet(),
+      preselect = cmp.PreselectMode.None,
       formatting = {
         fields = { 'abbr', 'menu' },
         format = function(entry, vim_item)
@@ -82,17 +95,11 @@ return {
         { name = 'path' },
         { name = 'crates' },
       },
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      -- experimental = {
-      --   ghost_text = true,
-      -- },
+    })
+
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } }),
     })
   end,
 }
