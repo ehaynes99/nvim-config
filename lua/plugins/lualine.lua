8baa730ae67484dc2860990f5046f335e24618bc
Eric Haynes <eric.haynes@openphone.co>
2023-01-26 10:37:16 -0500

    Remove path from statusline now that tree always shows path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,12 +4,7 @@ return {
     sections = {
       lualine_a = { 'mode' },
       lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = {
-        function()
-          return vim.fn.getcwd()
-        end,
-        { 'filename', newfile_status = true, path = 1 },
-      },
+      lualine_c = { 'filename', newfile_status = true, path = 1 },
       lualine_x = { 'encoding', 'fileformat', 'filetype' },
       lualine_y = { 'progress' },
       lualine_z = { 'location' },
