7a08cc0198b59394c584c6d96dbe71428d01e815
Eric Haynes <eric.haynes@openphone.co>
2023-02-19 22:19:43 -0500

    Move nvim-tree keymaps into config instead of making it lazy

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,59 +1,58 @@
 local editor_utils = require('utils.editor')
+local keymaps = require('keymaps')
 
 return {
   'nvim-tree/nvim-tree.lua',
-  lazy = false,
-  keys = {
-    { '<leader>e', ':NvimTreeFocus<CR>', desc = 'Focus tree view' },
-    { '<leader>E', ':NvimTreeToggle<CR>', desc = 'Open tree view' },
-    { '<leader>fw', editor_utils.search_tree_dir, { desc = 'Find: text within tree dir' } },
-  },
   config = function()
     require('nvim-tree').setup({
-  view = {
-    width = 40,
-    hide_root_folder = true,
-  },
-  update_focused_file = {
-    enable = true,
-    update_root = false,
-  },
-  -- open_on_setup = true,
-  sync_root_with_cwd = true,
-  -- open_on_setup_file = true,
-  diagnostics = {
-    enable = true,
-    show_on_dirs = true,
-    show_on_open_dirs = false,
-    icons = {
-      hint = '',
-      info = '',
-      warning = '',
-      error = '',
-    },
-  },
-  git = {
-    enable = true,
-    ignore = false,
-    show_on_dirs = true,
-    show_on_open_dirs = false,
-  },
-  renderer = {
-    icons = {
-      glyphs = {
-        git = {
-          untracked = 'U',
+      view = {
+        width = 40,
+        hide_root_folder = true,
+      },
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+      },
+      -- open_on_setup = true,
+      sync_root_with_cwd = true,
+      diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = false,
+        icons = {
+          hint = '',
+          info = '',
+          warning = '',
+          error = '',
         },
       },
-    },
-    highlight_git = true,
-  },
-  actions = {
-    change_dir = {
-      restrict_above_cwd = true,
-    },
-  },
-})
-    -- require('nvim-tree.api').tree.open()
+      git = {
+        enable = true,
+        ignore = false,
+        show_on_dirs = true,
+        show_on_open_dirs = false,
+      },
+      renderer = {
+        icons = {
+          glyphs = {
+            git = {
+              untracked = 'U',
+            },
+          },
+        },
+        highlight_git = true,
+      },
+      actions = {
+        change_dir = {
+          restrict_above_cwd = true,
+        },
+      },
+    })
+
+    keymaps.add({
+      { '<leader>e', ':NvimTreeFocus<CR>', { desc = 'Focus tree view' } },
+      { '<leader>E', ':NvimTreeToggle<CR>', { desc = 'Open tree view' } },
+      { '<leader>fw', editor_utils.search_tree_dir, { desc = 'Find: text within tree dir' } },
+    })
   end,
 }
