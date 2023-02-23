004ca94e962d7983a99afffaf9add5fcb8e84bdb
Eric Haynes <eric.haynes@openphone.co>
2023-02-23 00:14:14 -0500

    Update nvim-tree

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -53,3 +53,16 @@ vim.api.nvim_create_autocmd('BufReadPre', {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  pattern = 'NvimTree*',
+  callback = function()
+    local view = require('nvim-tree.view')
+    local is_visible = view.is_visible()
+
+    local api = require('nvim-tree.api')
+    if not is_visible then
+      api.tree.open()
+    end
+  end,
+})
