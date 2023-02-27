1c2c864ab1b6236841a17a4de1ea84c305ad7726
Eric Haynes <eric.haynes@openphone.co>
2023-02-26 20:49:03 -0500

    Add BufLeave to autosave triggers

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,7 +1,7 @@
 return {
   'Pocco81/auto-save.nvim',
   opts = {
-    trigger_events = { 'InsertLeave', 'TextChanged' },
+    trigger_events = { 'InsertLeave', 'TextChanged', 'BufLeave' },
     execution_message = {
       message = function()
         return ''
