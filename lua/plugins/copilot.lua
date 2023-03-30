dcc4bc4dcd36d8145cd1526e1985716d446802f8
Eric Haynes <eric.haynes@openphone.co>
2023-03-30 16:55:52 -0400

    Set up copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,25 @@
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup({
+      filetypes = {
+        ['*'] = false,
+        typescript = true,
+        rust = true,
+        lua = true,
+      },
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = 'kj',
+          next = '<A-j>',
+          prev = '<A-k>',
+          dismiss = 'kk',
+          accept_word = '<leader>kj',
+        },
+      },
+    })
+  end,
+}
