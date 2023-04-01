99fcc0056e05c2b096f51478e4d57ccad1d56f1d
Eric Haynes <eric.haynes@openphone.co>
2023-04-01 13:15:06 -0400

    Add refactoring plugin

---
+++ lua/plugins/refactoring.lua
@@ -0,0 +1,35 @@
+local keymaps = require('keymaps')
+local curry = require('utils.lua').curry
+
+return {
+  'ThePrimeagen/refactoring.nvim',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-treesitter/nvim-treesitter' },
+  },
+  config = function()
+    local refactoring = require('refactoring')
+    refactoring.setup({})
+
+    keymaps.add({
+      { '<leader>wr', refactoring.select_refactor, mode = 'x', { desc = 'Refactor: Select' } },
+      {
+        '<leader>we',
+        curry(refactoring.refactor)('Extract Variable'),
+        mode = 'v',
+        { desc = 'Refactor: Extract variable' },
+      },
+      {
+        '<leader>wi',
+        function()
+          refactoring.refactor('Inline Variable')
+          -- leave visual mode
+          local esc = vim.api.nvim_replace_termcodes('<esc>', true, false, true)
+          vim.api.nvim_feedkeys(esc, 'x', false)
+        end,
+        mode = 'v',
+        { desc = 'Refactor: Inline variable' },
+      },
+    })
+  end,
+}
