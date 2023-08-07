052611c4fa276ec7bc0042b4a0b7189eadeb5a45
Eric Haynes <eric.haynes@openphone.co>
2023-08-07 19:01:11 -0400

    Fix dressing location

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -3,11 +3,10 @@ return {
   event = 'VeryLazy',
   opts = {
     input = {
-      anchor = 'NW',
       override = function(conf)
         -- display under cursor, not on top of it
-        if conf.anchor == 'NW' and (conf.row == nil or conf.row == 0) then
-          conf.row = 1
+        if (conf.row == nil or conf.row == 0) then
+          conf.row = 4
         end
         return conf
       end,
