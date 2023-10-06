63127688137198965a1fe0c02d75d48584d2dc8d
Eric Haynes <eric.haynes@openphone.co>
2023-10-06 11:40:15 -0400

    Update plugins

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,16 +1,10 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
   config = function()
-    require('indent_blankline').setup()
-
-    vim.api.nvim_create_augroup('IndentBlankLineFix', {})
-    vim.api.nvim_create_autocmd('WinScrolled', {
-      group = 'IndentBlankLineFix',
-      callback = function()
-        if vim.v.event.all.leftcol ~= 0 then
-          vim.cmd('silent! IndentBlanklineRefresh')
-        end
-      end,
+    require('ibl').setup({
+      indent = {
+         char = '▏',
+      },
     })
   end,
 }
