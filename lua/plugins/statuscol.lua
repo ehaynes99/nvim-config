93f5b7e8725be52033ed749ee8f9511d8c31d2a4
Eric Haynes <eric.haynes@openphone.co>
2023-10-13 22:38:12 -0400

    Add room for diagnostic & breakpoint in status column

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -1,5 +1,6 @@
 return {
   'luukvbaal/statuscol.nvim',
+  enabled = false,
   config = function()
     local builtin = require('statuscol.builtin')
     require('statuscol').setup({
@@ -7,16 +8,21 @@ return {
       relculright = true,
       segments = {
         { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
-        -- {
-        --   sign = { name = { 'Diagnostic' }, maxwidth = 2, auto = true },
-        --   click = 'v:lua.ScSa',
-        -- },
-        { text = { builtin.lnumfunc } },
+        { text = { '%s' }, click = 'v:lua.ScSa' },
+        {
+          sign = { name = { 'Diagnostic' }, maxwidth = 1, auto = false },
+        },
+        -- { text = { builtin.lnumfunc } },
+        {
+          text = { builtin.lnumfunc},
+          condition = { true, builtin.not_empty },
+          click = 'v:lua.ScLa',
+        },
         -- {
         --   sign = { name = { '.*' }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
         --   click = 'v:lua.ScSa',
         -- },
-        { text = { ' ' } },
+        -- { text = { ' ' } },
       },
     })
   end,
