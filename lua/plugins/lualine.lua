3aeafdb093c5f9604c332badd3458204106a3c34
Eric Haynes <eric.haynes@openphone.co>
2023-10-18 15:24:45 -0400

    Add total lines to status bar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,11 +2,21 @@ return {
   'nvim-lualine/lualine.nvim',
   opts = {
     sections = {
-      lualine_a = { 'mode' },
+      -- lualine_a = { 'mode' },
+      lualine_a = {},
       lualine_b = { 'branch', 'diff', 'diagnostics' },
+      -- lualine_b = {},
       lualine_c = { { 'filename', newfile_status = true, path = 1 } },
-      lualine_x = { 'encoding', 'fileformat', 'filetype' },
-      lualine_y = { 'progress' },
+      -- lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_x = {},
+      lualine_y = {
+        function()
+          local line_count = vim.api.nvim_buf_line_count(0)
+          local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+          local padded = string.rep(' ', #tostring(line_count) - #tostring(row)) .. row
+          return ('line# %s/%d'):format(padded, line_count)
+        end,
+      },
       lualine_z = { 'location' },
     },
   },
