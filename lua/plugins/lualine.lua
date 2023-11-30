f11dc335de1c9a0c733fa44307013f209b9bc646
Eric Haynes <eric.haynes@openphone.co>
2023-11-29 20:40:21 -0500

    Display full path in status bar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
       -- lualine_a = { 'mode' },
       lualine_a = {},
       lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = { { 'filename', newfile_status = true, path = 1 } },
+      lualine_c = { { 'filename', newfile_status = true, path = 3 } },
       lualine_x = { 'encoding', 'fileformat', 'filetype' },
       lualine_y = {
         function()
