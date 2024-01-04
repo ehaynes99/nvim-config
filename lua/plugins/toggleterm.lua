f6735bbfc474d1f16bfad28922851c59137a151c
Eric Haynes <eric.haynes@openphone.co>
2024-01-04 15:53:02 -0500

    Always use default reporter for jest

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -62,7 +62,7 @@ return {
       end
 
       print('project_root: ' .. project_root)
-      local cmd = 'npx -y --node-options=--inspect jest --watch --no-coverage ' .. test_file
+      local cmd = 'npx -y --node-options=--inspect jest --reporters="default" --watch --no-coverage ' .. test_file
 
       if jest_term:is_open() then
         jest_term:change_dir(project_root)
