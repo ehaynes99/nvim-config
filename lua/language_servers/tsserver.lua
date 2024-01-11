fbbfc0012c4738b6b61c221ee3a5f58124ab9d2e
Eric Haynes <eric.haynes@openphone.co>
2024-01-10 22:22:55 -0500

    Revert changes to tsserver config. Eslint LSP is the problem

--- lua/language_servers/tsserver.lua
+++ lua/language_servers/tsserver.lua
@@ -1,60 +1,55 @@
-local keymaps = require('keymaps')
-local installer = require('language_servers.installer')
+return function()
+  local keymaps = require('keymaps')
+  local typescript = require('typescript')
+  local installer = require('language_servers.installer')
 
-return {
-  on_attach = function(_, bufnr)
-    keymaps.lsp_keymaps(bufnr) -- no formatter, uses eslint instead
+  typescript.setup({
+    server = {
+      on_attach = function(_, bufnr)
+        keymaps.lsp_keymaps(bufnr) -- no formatter, uses eslint instead
 
-    keymaps.add({
-      -- { 'gd', ':TypescriptGoToSourceDefinition<CR>', desc = 'LSP: Goto definition', buffer = bufnr },
-      {
-        '<leader>li',
-        ':TypescriptAddMissingImports<CR>',
-        desc = 'LSP: TypescriptAddMissingImports',
-        buffer = bufnr,
+        keymaps.add({
+          {
+            '<leader>li',
+            ':TypescriptAddMissingImports<CR>',
+            desc = 'LSP: TypescriptAddMissingImports',
+            buffer = bufnr,
+          },
+          {
+            '<leader>lo',
+            ':TypescriptOrganizeImports<CR>',
+            desc = 'LSP: TypescriptOrganizeImports',
+            buffer = bufnr,
+          },
+          { '<leader>lu', ':TypescriptRemoveUnused<CR>', desc = 'LSP: TypescriptRemoveUnused', buffer = bufnr },
+          { '<leader>lF', ':TypescriptFixAll<CR>', desc = 'LSP: TypescriptFixAll', buffer = bufnr },
+          { '<leader>lR', ':TypescriptRenameFile<CR>', desc = 'LSP: TypescriptRenameFile', buffer = bufnr },
+        })
+      end,
+      capabilities = installer.default_capabilities,
+      settings = {
+        diagnostics = {
+          ignoredCodes = {
+            -- Disable annoying diagnostics. All message codes:
+            -- https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+            6133, -- unused variable - eslint will handle
+            6138, -- unused property - eslint will handle
+            80001, -- convert to ES module suggestion
+            80005, -- 'require' call may be converted to an import
+            80006, -- may be converted to an async function -- auto-"fix" BREAKS CODE with multiple returns
+          },
+        },
+        completions = {
+          completeFunctionCalls = true,
+        },
       },
-      {
-        '<leader>lo',
-        ':TypescriptOrganizeImports<CR>',
-        desc = 'LSP: TypescriptOrganizeImports',
-        buffer = bufnr,
-      },
-      { '<leader>lu', ':TypescriptRemoveUnused<CR>', desc = 'LSP: TypescriptRemoveUnused', buffer = bufnr },
-      { '<leader>lF', ':TypescriptFixAll<CR>', desc = 'LSP: TypescriptFixAll', buffer = bufnr },
-      { '<leader>lR', ':TypescriptRenameFile<CR>', desc = 'LSP: TypescriptRenameFile', buffer = bufnr },
-    })
-  end,
-  capabilities = installer.default_capabilities,
-  settings = {
-    diagnostics = {
-      ignoredCodes = {
-        -- Disable annoying diagnostics. All message codes:
-        -- https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
-        6133, -- unused variable - eslint will handle
-        6138, -- unused property - eslint will handle
-        80001, -- convert to ES module suggestion
-        80005, -- 'require' call may be converted to an import
-        80006, -- may be converted to an async function -- auto-"fix" BREAKS CODE with multiple returns
+      init_options = {
+        preferences = {
+          importModuleSpecifierPreference = 'project-relative',
+          includeCompletionsWithSnippetText = true,
+          includeCompletionsForImportStatements = true,
+        },
       },
     },
-    completions = {
-      completeFunctionCalls = true,
-    },
-  },
-  init_options = {
-    preferences = {
-      importModuleSpecifierPreference = 'project-relative',
-      includeCompletionsWithSnippetText = true,
-      includeCompletionsForImportStatements = true,
-    },
-  },
-}
--- return function()
---   local keymaps = require('keymaps')
---   -- local typescript = require('typescript')
---   local installer = require('language_servers.installer')
---
---   typescript.setup({
---     server = ,
---   })
--- end
+  })
+end
