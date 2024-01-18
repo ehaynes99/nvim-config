b9d2c022a15f34dd2dd58a1bb0a5ab0aeec68e1d
Eric Haynes <eric.haynes@openphone.co>
2024-01-17 21:42:29 -0500

    Add keymap to go to next/previous reference

--- lua/plugins/vim-illuminate.lua
+++ lua/plugins/vim-illuminate.lua
@@ -1,7 +1,13 @@
 return {
   'RRethy/vim-illuminate',
   config = function()
+    local illuminate = require('illuminate')
     -- kanagawa "winterRed"
     vim.cmd('hi IlluminatedWordRead guibg=#43242B')
+
+    require('keymaps').add({
+      { '<leader>;', illuminate.goto_next_reference, desc = 'LSP: Go to next reference' },
+      { '<leader>:', illuminate.goto_prev_reference, desc = 'LSP: Go to previous reference' },
+    })
   end,
 }
