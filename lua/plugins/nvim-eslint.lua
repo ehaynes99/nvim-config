e6d57d4b43113e25e36f2c221dc0cc91d8769a88
Eric Haynes <eric.haynes@openphone.co>
2025-05-05 18:18:48 -0400

    fix eslint

--- lua/plugins/nvim-eslint.lua
+++ lua/plugins/nvim-eslint.lua
@@ -1,27 +1,26 @@
 return {
-  'ehaynes99/nvim-eslint',
-  -- dir = '/home/erich/workspace/ehaynes99/nvim-eslint',
+  -- 'esmuellert/nvim-eslint',
+  -- 'ehaynes99/nvim-eslint',
+  dir = '/home/erich/workspace/ehaynes99/nvim-eslint',
   config = function()
     local nvim = require('nvim-eslint')
     nvim.setup({
+      filetypes = { 'yaml' },
       settings = {
         format = true,
+        useFlatConfig = true,
         codeActionOnSave = {
           enable = false,
           mode = 'all',
         },
+        onIgnoredFiles = 'off',
       },
       on_attach = function(client, bufnr)
         client.server_capabilities.documentFormattingProvider = true
 
-        local format = function()
-          vim.lsp.buf.format({ timeout_ms = 2000 })
-        end
-        vim.keymap.set('n', 'ff', format, { silent = true, buffer = bufnr, desc = 'LSP: Format document' })
-        
         require('keymaps').lsp_keymaps(bufnr, function()
           print('formatting with eslint')
-          vim.lsp.buf.format({ timeout_ms = 2000 })
+          vim.lsp.buf.format({ timeout_ms = 5000 })
         end)
       end,
     })
