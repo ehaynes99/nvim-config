168663830a0c622c4c9b42be04172dd8183f4f6b
Eric Haynes <eric.haynes@openphone.co>
2025-06-30 15:52:51 -0400

    Remove outdate tree fix

--- lua/autocommands/init.lua
+++ lua/autocommands/init.lua
@@ -72,18 +72,18 @@ vim.api.nvim_create_autocmd('BufReadPre', {
   end,
 })
 
-vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-  pattern = 'NvimTree*',
-  callback = function()
-    local view = require('nvim-tree.view')
-    local is_visible = view.is_visible()
-
-    local api = require('nvim-tree.api')
-    if not is_visible then
-      api.tree.open()
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+--   pattern = 'NvimTree*',
+--   callback = function()
+--     local view = require('nvim-tree.view')
+--     local is_visible = view.is_visible()
+--
+--     local api = require('nvim-tree.api')
+--     if not is_visible then
+--       api.tree.open()
+--     end
+--   end,
+-- })
 
 vim.api.nvim_create_autocmd('FileType', {
   desc = 'Close buffers that will break with sessions',
