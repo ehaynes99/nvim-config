abc310a81647cc4aa1b7233480b42fce6d27f9b5
Eric Haynes <eric.haynes@openphone.co>
2025-06-29 23:46:51 -0400

    fix refactoring plugin

--- lua/plugins/refactoring.lua
+++ lua/plugins/refactoring.lua
@@ -1,35 +1,20 @@
-local keymaps = require('keymaps')
-local curry = require('utils.lua').curry
-
 return {
   'ThePrimeagen/refactoring.nvim',
   dependencies = {
     { 'nvim-lua/plenary.nvim' },
     { 'nvim-treesitter/nvim-treesitter' },
   },
-  config = function()
-    local refactoring = require('refactoring')
-    refactoring.setup({})
-
-    keymaps.add({
-      { '<leader>wr', refactoring.select_refactor, mode = 'x', desc = 'Refactor: Select' },
-      {
-        '<leader>we',
-        curry(refactoring.refactor)('Extract Variable'),
-        mode = 'v',
-        desc = 'Refactor: Extract variable',
-      },
-      {
-        '<leader>wi',
-        function()
-          refactoring.refactor('Inline Variable')
-          -- leave visual mode
-          local esc = vim.api.nvim_replace_termcodes('<esc>', true, false, true)
-          vim.api.nvim_feedkeys(esc, 'x', false)
-        end,
-        mode = 'v',
-        desc = 'Refactor: Inline variable',
-      },
-    })
-  end,
+  config = true,
+  keys = {
+    {
+      '<leader>wr',
+      function()
+        require('refactoring').select_refactor()
+      end,
+      mode = 'x',
+      desc = 'Refactor: Select',
+    },
+    { '<leader>we', ':Refactor extract_var ', mode = 'x', desc = 'Refactor: Extract variable' },
+    { '<leader>wi', ':Refactor inline_var', mode = {'n', 'x'}, desc = 'Refactor: Inline variable' },
+  },
 }
