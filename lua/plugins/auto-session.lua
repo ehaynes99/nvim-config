8b2ec8252d6bb17f9e2a936c2f2000007f92ec47
Eric Haynes <eric.haynes@openphone.co>
2025-11-08 20:55:37 -0500

    Add keybind to wipe session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,12 +1,22 @@
 return {
-  "rmagatti/auto-session",
+  'rmagatti/auto-session',
   lazy = false,
 
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
-  opts = {
-    suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-    -- log_level = 'debug',
-  },
+  config = function()
+    require('auto-session').setup({
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+      purge_after_minutes = 10080, -- 7 days
+    })
+
+    -- Delete session and quit without saving a new one
+    local delete_and_quit = function()
+      vim.cmd('AutoSession disable')
+      vim.cmd('AutoSession delete')
+      -- Disable auto-save to prevent saving a new session on quit
+      vim.cmd('qa')
+    end
+
+    vim.keymap.set('n', '<leader>xd', delete_and_quit, { desc = 'Delete auto-session and quit' })
+  end,
 }
