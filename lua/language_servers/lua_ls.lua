14dc96564434ddac5c5ec4db16c78b11e1da2333
Eric Haynes <eric.haynes@openphone.co>
2026-02-13 17:58:40 -0500

    Remove commented code

--- lua/language_servers/lua_ls.lua
+++ lua/language_servers/lua_ls.lua
@@ -1,25 +1,3 @@
--- local get_workspace = function()
---   local create_workspace = function(paths)
---     local result = {
---       library = {},
---       checkThirdParty = false,
---     }
---     for _, path in ipairs(paths) do
---       result.library[path] = true
---     end
---     return result
---   end
---
---   local cwd = vim.fn.getcwd()
---
---   local home_dir = vim.fn.expand('$HOME')
---   if cwd == home_dir .. '/.config/awesome' then
---     return create_workspace({
---       '/usr/share/awesome/lib',
---     })
---   end
--- end
-
 return {
   -- before_init = require('neodev.lsp').before_init,
   settings = {
@@ -31,7 +9,6 @@ return {
       diagnostics = {
         globals = { 'vim', 'awesome', 'client', 'screen', 'root' },
       },
-      -- workspace = get_workspace(),
       workspace = {
         library = vim.api.nvim_get_runtime_file('', true),
         checkThirdParty = false,
