660440fc8dcf84a16c570f068caa2229d72b04de
Eric Haynes <eric.haynes@openphone.co>
2026-04-27 19:22:18 -0400

    Fix hover borders

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -75,13 +75,7 @@ local native_lsp_config = function()
     },
   })
 
-  vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = 'rounded',
-  })
-
-  vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = 'rounded',
-  })
+  vim.o.winborder = 'rounded'
 end
 
 return {
