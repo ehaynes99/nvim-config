c3f46f2dc065d70f4e87c9fe53de9856e045337a
Eric Haynes <eric.haynes@openphone.co>
2026-05-08 15:57:35 -0400

    Remove conflicting keybind

--- lua/plugins/claudecode.lua
+++ lua/plugins/claudecode.lua
@@ -75,7 +75,6 @@ return {
     { '<leader>ac', '<cmd>ClaudeCode<cr>', desc = 'Toggle Claude' },
     { '<leader>af', '<cmd>ClaudeCodeFocus<cr>', desc = 'Focus Claude' },
     { '<leader>ar', '<cmd>ClaudeCode --resume<cr>', desc = 'Resume Claude' },
-    { '<leader>aC', '<cmd>ClaudeCode --continue<cr>', desc = 'Continue Claude' },
     { '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', desc = 'Select Claude model' },
     { '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', desc = 'Add current buffer' },
     { '<leader>as', '<cmd>ClaudeCodeSend<cr>', mode = 'x', desc = 'Send selection to Claude' },
