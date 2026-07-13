1061500c460b35056b67486c19d5d4fc337e646e
Eric Haynes <eric.haynes@openphone.co>
2026-07-13 19:42:04 -0400

    Open github link on default branch

--- lua/plugins/snacks/init.lua
+++ lua/plugins/snacks/init.lua
@@ -1,3 +1,9 @@
+local function default_branch()
+  local out = vim.fn.systemlist('git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null')
+  if vim.v.shell_error ~= 0 or #out == 0 then return nil end
+  return out[1]:match('refs/remotes/origin/(.+)')
+end
+
 return {
   'folke/snacks.nvim',
   priority = 1000,
@@ -11,8 +17,8 @@ return {
     zen = { win = { width = 0.9, backdrop = { blend = 90 } } },
   },
   keys = {
-    { '<leader>gf', function() Snacks.gitbrowse() end, mode = { 'n', 'x' }, desc = 'Git: open selected lines in github' },
-    { '<leader>gF', function() Snacks.gitbrowse({ what = 'file' }) end, mode = { 'n', 'x' }, desc = 'Git: open file in github' },
+    { '<leader>gf', function() Snacks.gitbrowse({ branch = default_branch() }) end, mode = { 'n', 'x' }, desc = 'Git: open selected lines in github' },
+    { '<leader>gF', function() Snacks.gitbrowse({ what = 'file', branch = default_branch() }) end, mode = { 'n', 'x' }, desc = 'Git: open file in github' },
     { '<leader>wm', function() Snacks.zen() end, desc = 'Toggle zen mode' },
     { '<A-m>', function() Snacks.zen() end, mode = { 'n', 'i' }, desc = 'Toggle zen mode' },
   },
