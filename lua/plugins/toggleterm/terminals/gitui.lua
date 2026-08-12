02c84b4db002e7679f82223853f7381fc5d37f9d
Eric Haynes <eric.haynes@openphone.co>
2026-08-12 14:52:31 -0400

    use gitui for dotfiles

--- lua/plugins/toggleterm/terminals/gitui.lua
+++ lua/plugins/toggleterm/terminals/gitui.lua
@@ -1,6 +1,31 @@
 local project_utils = require('utils.project')
 local terminal_utils = require('plugins.toggleterm.terminals.terminal_utils')
 
+-- Returns path relative to home, '' if path is home, or nil if path isn't under home.
+local function relative_to_home(path, home)
+  if path == home then
+    return ''
+  end
+  local prefix = home .. '/'
+  if path:sub(1, #prefix) == prefix then
+    return path:sub(#prefix + 1)
+  end
+  return nil
+end
+
+-- Mirrors `dit rev-parse --verify --quiet HEAD:<path>`, checking whether
+-- project_dir is tracked in the bare dotfiles repo (see `dit`/`ditui` aliases).
+local function is_dotfiles_tracked(project_dir, home)
+  local relpath = relative_to_home(project_dir, home)
+  if relpath == nil then
+    return false
+  end
+
+  local ref = relpath == '' and 'HEAD' or ('HEAD:' .. relpath)
+  vim.fn.system({ 'git', '--git-dir', home .. '/.dotfiles.git', 'rev-parse', '--verify', '--quiet', ref })
+  return vim.v.shell_error == 0
+end
+
 return function(Terminal)
   local term = Terminal:new({
     hidden = true,
@@ -15,8 +40,14 @@ return function(Terminal)
   })
 
   local toggle = function()
-    local git_root = project_utils.git_root() or vim.fn.getcwd()
-    term.cmd = 'gitui -d ' .. git_root
+    local project_dir = project_utils.git_root() or vim.fn.getcwd()
+    local home = vim.fn.expand('$HOME')
+
+    if is_dotfiles_tracked(project_dir, home) then
+      term.cmd = 'gitui -d ' .. home .. '/.dotfiles.git/ -w ' .. home
+    else
+      term.cmd = 'gitui -d ' .. project_dir
+    end
     term:toggle()
   end
 
